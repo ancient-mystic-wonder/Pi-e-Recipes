e77d5b7ea89cb42920799ed6d54e1d45ce0c5af6
Ancient Mystic Wonder <ancient.mystic.wonder@gmail.com>
2017-01-20 00:43:36 +0800

    [UNTESTED] Added dnsmasq and iptables.

--- pi-to-router.sh
+++ pi-to-router.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 
+## Original tutorial: https://frillip.com/using-your-raspberry-pi-3-as-a-wifi-access-point-with-hostapd/
+## https://webcache.googleusercontent.com/search?q=cache:8nfWTN8xUhwJ:https://frillip.com/using-your-raspberry-pi-3-as-a-wifi-access-point-with-hostapd/+&cd=1&hl=en&ct=clnk&gl=ph
+
 DHCP_CONF="etc/dhcpcd.conf"
 NETWORK_INTERFACE_FILE="etc/network/interfaces"
+HOSTAPD_CONF_FILE="etc/hostapd/hostapd.conf"
+HOSTAPD_DEFAULT_CONF_FILE="etc/default/hostapd"
+DNSMASQ_CONF_FILE="etc/dnsmasq.conf"
+
+
 IP_ADDR="192.168.1.100"
 NETMASK="255.255.255.0"
 NETWORK="192.168.1.1"
 BROADCAST="192.168.1.255"
 
+IFACE_IN="eth0"
 IFACE_OUT="wlan0"
 PI_ROUTER_SSID="Pi3-AP"
 PI_ROUTER_PASSWORD="raspberry"
+DHCP_RANGE_START="192.168.1.50"
+DHCP_RANGE_END="192.168.1.150"
 
 ## Deny wlan interface which will be used for outgoing traffic.
 to_add="denyinterfaces wlan0"
@@ -33,4 +44,18 @@ sed -i.bak "s/$iface_dynamic/$iface_static/" "$NETWORK_INTERFACE_FILE"
 ## Open template hostapd.conf file and do replacements
 sed -e "s/\${IFACE_OUT}/$IFACE_OUT/" \
 	-e "s/\${PI_ROUTER_SSID}/$PI_ROUTER_SSID/" \
-	-e "s/\${PI_ROUTER_PASSWORD}/$PI_ROUTER_PASSWORD/" hostapd-template.conf > etc/hostapd/hostapd.conf
+	-e "s/\${PI_ROUTER_PASSWORD}/$PI_ROUTER_PASSWORD/" hostapd-template.conf > $HOSTAPD_CONF_FILE
+
+## Edit hostapd default configuration file for startup
+sed -i.bak 's|.*DAEMON_CONF=".*|DAEMON_CONF="/etc/hostapd/hostapd.conf"|' $HOSTAPD_DEFAULT_CONF_FILE
+
+## Open template dnsmasq.conf file and do replacements
+sed -e "s/\${IFACE_OUT}/$IFACE_OUT/" \
+	-e "s/\${IP_ADDR}/$IP_ADDR/" \
+	-e "s/\${DHCP_RANGE_START}/$DHCP_RANGE_START/" \
+	-e "s/\${DHCP_RANGE_END}/$DHCP_RANGE_END/" dnsmasq-template.conf > $DNSMASQ_CONF_FILE
+
+# Setup iptables
+iptables -t nat -A POSTROUTING -o $IFACE_IN -j MASQUERADE
+iptables -A FORWARD -i $IFACE_IN -o $IFACE_OUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i $IFACE_OUT -o $IFACE_IN -j ACCEPT
